b201b2d98ac548866ee544c8a263f797a79f2d10
mog-wk <ms-work@mail.com>
2023-05-28 13:54:39 -0300

    added peek, pop functions and Debug trait

--- data-structures/stack/stack.rs
+++ data-structures/stack/stack.rs
@@ -1,17 +1,40 @@
 use std::ops::Add;
-use std::fmt::Display;
+use std::fmt::{ Display, Debug };
+use std::clone::Clone;
 
 struct Stack<T> {
     list: Vec<T>,
+    len: usize,
 }
 
-impl<T> Stack<T> {
-    fn new(list: Vec<T>) -> Stack<T> {
-        Stack {list: list.to_vec()}
+impl<T: Clone> Stack<T> {
+    fn new(list: &[T]) -> Stack<T> {
+        Stack {list: list.to_vec(), len: list.len()}
     }
 
     fn push(&mut self, elt: T) {
+        self.len += 1;
         self.list.push(elt);
+    }
+
+    fn extend() {
+
+    }
+
+    fn peek(&self) -> Result<&T, &str> {
+        if self.len < 1 {
+            return Err("can not peek empty stack")
+        } else {
+            Ok(&self.list[self.len - 1])
+        }
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None
+        }
+        self.len -= 1;
+        self.list.pop()
     }
 
 }
@@ -20,7 +43,7 @@ impl<T> Stack<T> {
 impl<T> Add for Stack<T> where Vec<T>: Add<Output = Vec<T>> {
     type Output = Stack<T>;
     fn add(self, other: Stack<T>) -> Self::Output {
-        Stack {list: self.list + other.list}
+        Stack {list: self.list + other.list, len: self.len + other.len}
     }
 }
 
@@ -28,10 +51,16 @@ impl<T: std::fmt::Display> Display for Stack<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "Stack: [")?;
 
-        for (i, e) in self.list.iter().enumerate() {
-            write!(f, "{} {}", i, e);
+        for i in self.list.iter() {
+            write!(f, "{}", i)?;
         }
         write!(f, "]")
+    }
+}
+
+impl<T: std::fmt::Debug> Debug for Stack<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self.list)
     }
 }
 
@@ -39,14 +68,23 @@ impl<T: std::fmt::Display> Display for Stack<T> {
 fn main() {
 
     let tests = [
-        Stack::new([1, 2, 3, 4]),
-        Stack::new([5, 2, 3, 1]),
-        Stack::new([6, 2, 1, 12]),
-        Stack::new([5, 2]),
-        Stack::new([]),
+        Stack::new(&[1, 2, 3, 4]),
+        Stack::new(&[5, 2, 3, 1]),
+        Stack::new(&[6, 2, 1, 12]),
+        Stack::new(&[5, 2]),
+        Stack::new(&[]),
     ];
 
-    println!("{:?}", stack_1.list);
+    println!("data set: {:?}\n", tests);
+
+    for mut t in tests {
+        println!("init => {:?}", t);
+        t.push(123);
+        println!("{}", t.peek().unwrap());
+        t.pop();
+        println!("{}", t.peek().unwrap());
+        println!("end => {:?}\n", t);
+    }
 
 }
 
