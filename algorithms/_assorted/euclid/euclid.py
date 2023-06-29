834c5d77d7558d3060554a0cdb29377ec0312728
mog-wk <ms-work@mail.com>
2023-06-29 15:19:14 -0300

    fixed euclid.py

--- algorithms/_assorted/euclid/euclid.py
+++ algorithms/_assorted/euclid/euclid.py
@@ -1,18 +1,11 @@
+import math
 
-def euclid(a: int , b: int, verbose: bool = False) -> int:
-    if b > a: a, b = b, a
-    if b == 0: return -1
-
-    n = abs(a)
-    d = abs(b)
-
-    while d != 0:
-        mod = n % d
-        n = d
-        d = mod
-        if verbose == True: print(n, d)
-
-    return n
+def euclid(m: int, n: int) -> int:
+    if n > m: (m, n) = (n, m)
+    if n == 0: return m;
+    while m % n != 0:
+        (m, n) = (n, m % n)
+    return abs(n)
 
 
 if __name__ == "__main__":
@@ -23,13 +16,15 @@ if __name__ == "__main__":
         ((270, 192), 6),
         ((16_156, 32), 4),
         ((523, 0), -1),
+        ((6, 17), -1),
         ((42, 5), 1),
         ((25, 125), 25),
-        ((0, 5), -1),
+        ((12, 50), 2),
+        ((0, 5), 5),
+        ((0, 0), 0),
         ]
 
     for inp, res in tests:
         a, b = inp
-        euclid(a, b)
-        print("{} => {}" .format(euclid(a, b, ), res))
+        print("input: {} result: {} answer: {}" .format(inp, euclid(a, b), math.gcd(a, b)))
 
