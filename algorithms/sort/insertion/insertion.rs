6b5d0c522317a5bb0cacf4dcda2eacd46bde5974
mog-wk <ms-work@mail.com>
2023-08-12 08:33:18 -0300

    made insert rust inplace

--- algorithms/sort/insertion/insertion.rs
+++ algorithms/sort/insertion/insertion.rs
@@ -1,21 +1,20 @@
 
-
 fn main() {
     println!("Hello, world!");
 
-    let test = vec![5, 2, 3, 4, 1];
-    println!("{:?}", issort(&test));
+    let mut test = vec![5, 2, 3, 4, 1];
+    issort(&mut test, 5);
+    println!("{:?}", test);
 }
 
-fn issort(arr: &[u32]) -> Vec<u32> {
-    let mut sorted = arr.to_vec();
-    println!("{:?}", sorted);
-    for i in 1..arr.len() {
+fn issort(arr: &mut [u32], sz: usize) {
+    println!("{:?} {}", arr, sz);
+    for i in 1..sz {
         let mut j = i;
-        while j > 0 && &sorted[j] < &sorted[j-1] {
-            sorted.swap(j, j-1);
+        while j > 0 && arr[j] < arr[j - 1] {
+            println!("{} {}", i, j);
+            arr.swap(j, j - 1);
             j-=1;
         }
     }
-    sorted
 }
